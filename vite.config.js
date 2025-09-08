4adc87cffd75bcf0ff031d325442a6611f817a3f
pricile448 <ismoconnect@example.com>
2025-09-08 19:09:04 +0100

    Add project configuration files and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,17 +7,5 @@ export default defineConfig({
   server: {
     port: 3001,
     open: true
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          firebase: ['firebase/app', 'firebase/firestore', 'firebase/auth']
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['firebase/app', 'firebase/firestore', 'firebase/auth']
   }
 })
